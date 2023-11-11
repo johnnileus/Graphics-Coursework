033d140ac645e097260c9b28ad4e511325a8450b
gvidas <danieliusgvidas@gmail.com>
2023-11-11 22:18:44 +0000

    refreshed project with new urp and added lens flare

--- Assets/Scripts/DayNightController.cs
+++ Assets/Scripts/DayNightController.cs
@@ -7,16 +7,21 @@ public class DayNightController : MonoBehaviour{
     [SerializeField] private float currentVal = 0;
     
     public Gradient fogColour;
+
+    private float defaultYRot;
+    private float defaultZRot;
     
     // Start is called before the first frame update
     void Start(){
- 
+        defaultYRot = transform.eulerAngles.y;
+        defaultZRot = transform.eulerAngles.z;
+        print(defaultYRot);
     }
 
     // Update is called once per frame
     void Update(){
         currentVal = (currentVal + 0.01f * Time.deltaTime) % 1f;
-        transform.rotation = Quaternion.Euler(currentVal*360,60, 60);
+        transform.eulerAngles = new Vector3(currentVal*360,defaultYRot, defaultZRot);
         RenderSettings.fogColor = fogColour.Evaluate(currentVal);
     }  
 }
