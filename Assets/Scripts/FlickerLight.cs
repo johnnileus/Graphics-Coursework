c7491e220e1277ac4c2f32448c682c78a4ffff84
gvidas <danieliusgvidas@gmail.com>
2023-11-16 15:25:03 +0000

    plane

--- Assets/Scripts/FlickerLight.cs
+++ Assets/Scripts/FlickerLight.cs
@@ -2,8 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlickerLight : MonoBehaviour
-{
+public class FlickerLight : MonoBehaviour{
+
+    [SerializeField] private AnimationCurve intensity;
+    [SerializeField] private Light light;
+
+    public float flickerStrength;
+    public float flickerOffset;
+
+    private float curVal;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,10 @@ public class FlickerLight : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        curVal = (curVal + 0.1f * Time.deltaTime) % 1f;
+        //perlinnoise returns value 0-1 TODO: add another layer of noise
+        float val1 = Mathf.PerlinNoise(curVal*40, 1);
+        float val2 =  Mathf.PerlinNoise(curVal * 160, 100) * 0.2f;
+        light.intensity = val1 + val2;
     }
 }
